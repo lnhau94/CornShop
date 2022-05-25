43dfa308b05bc967b8153f610dc592487551b69b
pndhuy150902 <phamduchuy159@gmail.com>
2022-05-25 20:39:06 +0700

    Huy push code

---
+++ Statistic/ReportEndDay/Main.java
@@ -0,0 +1,30 @@
+package Statistic.ReportEndDay;
+
+import java.io.File;
+
+import javafx.application.Application;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Parent;
+import javafx.scene.Scene;
+import javafx.stage.Stage;
+
+public class Main extends Application{
+    public void start(Stage primaryStage){
+        try {
+            FXMLLoader fxmlLoader = new FXMLLoader();
+            fxmlLoader.setLocation(new File("src/Statistic/ReportEndDay/View/ReportEndDay.fxml").toURI().toURL());
+            Parent root = (Parent) fxmlLoader.load();
+            Scene scene = new Scene(root);
+            scene.getStylesheets().add(new File("src/Statistic/ReportEndDay/View/CSS/ReportEndDay.css").toURI().toURL().toExternalForm());
+            primaryStage.setTitle("END OF DAY REPORT");
+            primaryStage.setScene(scene);
+            primaryStage.show();
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+    }
+
+    public static void main(String[] args) throws Exception {
+        launch(args);
+    }
+}
