6f7e8871c829ca1ef0c0990f0cc2a0ee197cd094
Hau <lnhau94@gmail.com>
2022-05-25 20:53:02 +0700

    account

---
+++ Entity/DataStore.java
@@ -0,0 +1,21 @@
+package Entity;
+
+import Entity.Entity.*;
+import javafx.collections.ObservableList;
+
+public class DataStore {
+    public static ObservableList<Account> accounts;
+    public static ObservableList<Brand> brands;
+    public static ObservableList<Category> categories;
+    public static ObservableList<Color> colors;
+    public static ObservableList<Customer> customers;
+    public static ObservableList<Employee> employees;
+    public static ObservableList<IncomeBill> incomeBills;
+    public static ObservableList<IncomeDetails> incomeDetails;
+    public static ObservableList<Order> orders;
+    public static ObservableList<OrderDetails> orderDetails;
+    public static ObservableList<Product> products;
+    public static ObservableList<Size> sizes;
+    public static ObservableList<Storage> storages;
+    public static ObservableList<Supplier> suppliers;
+}
---
+++ Manager/AccountManager/Controller.java
@@ -0,0 +1,4 @@
+package Manager.AccountManager;
+
+public class Controller {
+}
