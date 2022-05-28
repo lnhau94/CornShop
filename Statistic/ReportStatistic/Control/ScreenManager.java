3bb59bd3462a7aa965a7c167675e2a9c15ad7492
pndhuy150902 <phamduchuy159@gmail.com>
2022-05-28 09:22:41 +0700

    Huy push code

--- Statistic/ReportStatistic/Control/ScreenManager.java
+++ Statistic/ReportStatistic/Control/ScreenManager.java
@@ -17,33 +17,33 @@ public class ScreenManager {
 
     public void screenRevenueStatistic(ActionEvent event) throws IOException {
         FXMLLoader fxmlLoader = new FXMLLoader();
-        fxmlLoader.setLocation(new File("Statistic/ReportStatistic/View/RevenueStatistic.fxml").toURI().toURL());
+        fxmlLoader.setLocation(new File("src/Statistic/ReportStatistic/View/RevenueStatistic.fxml").toURI().toURL());
         root = (Parent) fxmlLoader.load();
         stage = (Stage)((Node)event.getSource()).getScene().getWindow();
         scene = new Scene(root);
-        scene.getStylesheets().add(new File("Statistic/ReportStatistic/View/CSS/RevenueStatistic.css").toURI().toURL().toExternalForm());
+        scene.getStylesheets().add(new File("src/Statistic/ReportStatistic/View/CSS/RevenueStatistic.css").toURI().toURL().toExternalForm());
         stage.setScene(scene);
         stage.show();
     }
 
     public void screenProductStatistic(ActionEvent event) throws IOException {
         FXMLLoader fxmlLoader = new FXMLLoader();
-        fxmlLoader.setLocation(new File("Statistic/ReportStatistic/View/ProductStatistic.fxml").toURI().toURL());
+        fxmlLoader.setLocation(new File("src/Statistic/ReportStatistic/View/ProductStatistic.fxml").toURI().toURL());
         root = (Parent) fxmlLoader.load();
         stage = (Stage)((Node)event.getSource()).getScene().getWindow();
         scene = new Scene(root);
-        scene.getStylesheets().add(new File("Statistic/ReportStatistic/View/CSS/ProductStatistic.css").toURI().toURL().toExternalForm());
+        scene.getStylesheets().add(new File("src/Statistic/ReportStatistic/View/CSS/ProductStatistic.css").toURI().toURL().toExternalForm());
         stage.setScene(scene);
         stage.show();
     }
 
     public void screenCategoryStatistic(ActionEvent event) throws IOException {
         FXMLLoader fxmlLoader = new FXMLLoader();
-        fxmlLoader.setLocation(new File("Statistic/ReportStatistic/View/CategoryStatistic.fxml").toURI().toURL());
+        fxmlLoader.setLocation(new File("src/Statistic/ReportStatistic/View/CategoryStatistic.fxml").toURI().toURL());
         root = (Parent) fxmlLoader.load();
         stage = (Stage)((Node)event.getSource()).getScene().getWindow();
         scene = new Scene(root);
-        scene.getStylesheets().add(new File("Statistic/ReportStatistic/View/CSS/CategoryStatistic.css").toURI().toURL().toExternalForm());
+        scene.getStylesheets().add(new File("src/Statistic/ReportStatistic/View/CSS/CategoryStatistic.css").toURI().toURL().toExternalForm());
         stage.setScene(scene);
         stage.show();
     }
